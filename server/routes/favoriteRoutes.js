6742156bb679ec5981e8d1f514f521de3a7570d0
Grayson Siver <graysonsiver88@gmail.com>
2026-07-23 18:12:00 -0500

    Added the REST Client .http file so we can run the api tests while using npm run dev, also completed the favoriteRoutes.js and added the correct import to server.js

--- server/routes/favoriteRoutes.js
+++ server/routes/favoriteRoutes.js
@@ -1,3 +1,11 @@
 import express from "express";
 import { getFavorites, addFavorite, removeFavorite } from "../controllers/favoriteController.js";
-// import authenticate... will finish when back from work.
+// import authenticateToken from "../middleware/authMiddleware.js"; // added when auth pushes
+
+const router = express.Router();
+
+router.get("/", /* authenticateToken, */ getFavorites);
+router.post("/", /* authenticateToken, */ addFavorite);
+router.delete("/:id", /*authenticateToken, */ removeFavorite);
+
+export default router;
