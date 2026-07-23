b18a2a37e80d0e4e3ff22c228220d690ab49596f
Alyssa Scott <lilzkin4@gmail.com>
2026-07-22 22:07:35 -0500

    Complete homepage hero, search, and filter layout

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero'
 import SearchBar from "../components/SearchBar";
+import FilterBar from "../components/FilterBar";
 
 function Home() {
   return (
     <main>
       <Hero />
       <SearchBar />
+      <FilterBar />
     </main>
   );
 }
