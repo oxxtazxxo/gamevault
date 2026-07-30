3c3a7a3bb64d2c1d6005c916f9b9257dca21958e
Lucas Brown <lucasbrown124@gmail.com>
2026-07-30 15:52:09 -0500

    Revert "added logger middleware"
    This reverts commit b0e761342c71359301fab08a375c6b8bae6be928.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ import dotenv from "dotenv";
 import { rawgRouter } from './routes/rawg.js'
 import connectDB from "./config/db.js";
 import favoriteRoutes from "./routes/favoriteRoutes.js";
-import { logger } from "./middleware/logger.js";
 
 dotenv.config();
 connectDB();
@@ -14,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(logger);
 app.use('/api/rawg', rawgRouter);
 
 
