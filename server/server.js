7cd21aa64d504308fa50b591ed124fad680fad0a
Munakata709 <frankytejeda78@gmail.com>
2026-07-30 23:15:44 -0400

    restored all of devlops missing files into auth

--- server/server.js
+++ server/server.js
@@ -1,32 +1,31 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
-dotenv.config();
-
+import { rawgRouter } from './routes/rawg.js'
 import connectDB from "./config/db.js";
 import favoriteRoutes from "./routes/favoriteRoutes.js";
-import { rawgRouter } from './routes/rawg.js';
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from './routes/authRoutes.js';
 
+dotenv.config();
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
-//auth 
-app.use('/api/auth', authRoutes);
-
-
 app.use('/api/rawg', rawgRouter);
-app.use("/api/favorites", favoriteRoutes);
+
 
 app.get("/", (req, res) => {
   res.json({ message: "GameVault API is running!" });
 });
+
+// teamates add their own line here as routes are built
+app.use('/api/auth', authRoutes);
+// app.user ("/api/games", gameRoutes);
+// (for example)
+app.use("/api/favorites", favoriteRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
