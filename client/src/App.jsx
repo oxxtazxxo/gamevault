d8e54b950cf61aff8f958855d14e8f5d747d9812
Alyssa Scott <lilzkin4@gmail.com>
2026-07-30 22:57:37 -0500

    Enhance frontend with footer, filter icons, and sparkle cursor foundation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
+import Footer from "./components/Footer.jsx";
+import SparkleCursor from "./components/SparkleCursor";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Home />
-    </>
-  );
-}
+    return (
+        <div className="app-container">
+            <Navbar />
+            <Home />
+            <Footer />
 
+            <SparkleCursor />
+        </div>
+    );
+}
 export default App;
