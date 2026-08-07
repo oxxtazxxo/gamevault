c1eeea4b1ceb6ecd9c69034e31d87384453184a3
Grayson Siver <graysonsiver88@gmail.com>
2026-08-06 21:24:06 -0500

    uncommented the code from the favoriteRoutes.js

--- server/routes/favoriteRoutes.js
+++ server/routes/favoriteRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { getFavorites, addFavorite, removeFavorite } from "../controllers/favoriteController.js";
-// import authenticateToken from "../middleware/authMiddleware.js"; // added when auth pushes
+import authenticateToken from "../middleware/authMiddleware.js"; // added when auth pushes
 
 const router = express.Router();
 
-router.get("/", /* authenticateToken, */ getFavorites);
-router.post("/", /* authenticateToken, */ addFavorite);
-router.delete("/:id", /*authenticateToken, */ removeFavorite);
+router.get("/", authenticateToken, getFavorites);
+router.post("/", authenticateToken, addFavorite);
+router.delete("/:id", authenticateToken, removeFavorite);
 
 export default router;
